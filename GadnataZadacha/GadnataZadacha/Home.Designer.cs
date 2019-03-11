d8acf3579230f94ed3924fd4925574b1c86a9c9b
Shaban <shaban9726@gmail.com>
2019-03-11 09:04:08 +0200

    Added buttno Register to Home form

--- GadnataZadacha/GadnataZadacha/Home.Designer.cs
+++ GadnataZadacha/GadnataZadacha/Home.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -50,15 +51,25 @@
             this.button2.Text = "Login";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 59);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Register";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Name = "Home";
             this.Text = "LoginForm";
             this.ResumeLayout(false);
 
@@ -68,6 +79,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
