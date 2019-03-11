7f920d07c4f5f780f5164fa519c2b15390b04342
Shaban <shaban9726@gmail.com>
2019-03-11 09:02:28 +0200

    Created Lofgin form Created Home form Created login/logout buttons Started creating Login labels

--- GadnataZadacha/GadnataZadacha/Login.cs
+++ GadnataZadacha/GadnataZadacha/Login.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace GadnataZadacha
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- GadnataZadacha/GadnataZadacha/Program.cs
+++ GadnataZadacha/GadnataZadacha/Program.cs
@@ -16,7 +16,7 @@ namespace GadnataZadacha
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Home());
         }
     }
 }
